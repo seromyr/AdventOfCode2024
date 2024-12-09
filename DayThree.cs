cad147e67e4889bd919a996e471c84e4d3a4dcf9
seromyr <55792226+seromyr@users.noreply.github.com>
2024-12-09 01:19:04 -0400

    Update Day Three Part I Solution

---
+++ DayThree.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024;
+
+public class DayThree : DaySolution
+{
+    public DayThree(string day, string inputData) : base(day, inputData)
+    {
+        Day = day;
+        InputData = inputData;
+    }
+
+    private string[] exportData;
+    private List<int> _mults = new List<int>();
+
+    // Regex pattern to match "mul(?,?)" where ? is one or more digits
+    string _pattern = @"mul\((\d+),(\d+)\)";
+
+    protected override void ProcessData()
+    {
+        base.ProcessData();
+
+        ParseInputData(InputData, "Parsing input data into reports...");
+        Console.WriteLine($"The total of the multiplications is {GetTotalMultiplications()}");
+    }
+
+    protected override void ParseEachLine(string line)
+    {
+        exportData = line.Split();
+
+        foreach (string part in exportData)
+        {
+            MatchCollection matches = Regex.Matches(part, _pattern);
+
+            foreach (Match match in matches)
+            {
+                int mult = ConvertToNumber(match.Groups[1].Value) * ConvertToNumber(match.Groups[2].Value);
+                _mults.Add(mult);
+            }
+        }
+    }
+
+    private int GetTotalMultiplications()
+    {
+        int result = 0;
+
+        foreach (var t in _mults)
+        {
+            result += t;
+        }
+
+        return result;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using AdventOfCode2024;
 
 DayOne dayOneResult = new ("One", "Data/day-one-input.txt");
 DayTwo dayTwoResult = new ("Two","Data/day-two-input.txt");
+DayThree dayThreeResult = new ("Three","Data/day-three-input.txt");
 
 Console.WriteLine("Hello, World!");
-dayOneResult.ShowResult();
-dayTwoResult.ShowResult();
+// dayOneResult.ShowResult();
+// dayTwoResult.ShowResult();
+dayThreeResult.ShowResult();
 Console.ReadLine();
